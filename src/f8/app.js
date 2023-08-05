7935744b834376010f8867705e380b232d2ad64f
source3thaipvt <source3thaipvt@gmail.com>
2023-08-05 15:27:26 +0700

    create nodejs and install express

---
+++ src/f8/app.js
@@ -0,0 +1,12 @@
+// Expressjs là một framework được xây dựng trên nền tảng của Nodejs. Nó cung cấp các tính năng mạnh mẽ để phát triển web hoặc mobile. Expressjs hỗ trợ các method HTTP và midleware tạo ra API vô cùng mạnh mẽ và dễ sử dụng.
+const express = require('express')
+const app = express()
+const port = 3000
+// route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
